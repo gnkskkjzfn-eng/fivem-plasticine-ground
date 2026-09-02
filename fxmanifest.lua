34cf097a929a62a1a128f41e4fb0c21576ed335a
gnkskkjzfn-eng <gnkskkjzfn@privaterelay.appleid.com>
2026-09-02 19:28:30 +0300

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Plasticine Ground Effect'
+description 'FiveM client-side plasticine ground effect - smooth terrain shader'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+files {
+    'shaders/plasticine.fx'
+}
+
+provide 'plasticine_ground'
