58dee9f94668579ae2f0e1380c47e96d160c22f8
gnkskkjzfn-eng <gnkskkjzfn@privaterelay.appleid.com>
2026-09-02 19:37:17 +0300

    Fix fxmanifest.lua with proper ScreenFX reference

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Plasticine Ground Effect'
-description 'FiveM client-side plasticine ground effect - smooth terrain shader'
+description 'FiveM client-side plasticine ground effect using screen-space post-processing'
 version '1.0.0'
 
 client_scripts {
@@ -11,7 +11,15 @@ client_scripts {
 }
 
 files {
-    'shaders/plasticine.fx'
+    'shaders/plasticine_screen.fx'
+}
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/ui.js',
+    'html/ui.css'
 }
 
 provide 'plasticine_ground'
